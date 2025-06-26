40e699b829ccc3a052545b13bdc898b7ee150b81
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-06-26 18:09:48 -0400

    test: update UtilsTest for 4.56.0

--- src/test/java/com/atlasgong/invisibleitemframeslite/UtilsTest.java
+++ src/test/java/com/atlasgong/invisibleitemframeslite/UtilsTest.java
@@ -1,6 +1,5 @@
 package com.atlasgong.invisibleitemframeslite;
 
-import com.atlasgong.invisibleitemframeslite.itemframe.ItemFrameRegistry;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -8,14 +7,13 @@ import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.inventory.ItemStack;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockbukkit.mockbukkit.MockBukkit;
 import org.mockbukkit.mockbukkit.ServerMock;
-import org.mockbukkit.mockbukkit.entity.PlayerMock;
 
 import java.lang.reflect.Field;
 
@@ -47,12 +45,24 @@ class UtilsTest {
     }
 
     @Test
-    void testIsInvisibleItemFrameWithItemStack() {
-        ItemStack is = ItemFrameRegistry.getInstance().getRegularInvisibleFrame();
-        assert Utils.isInvisibleItemFrame(is, isInvisibleKey);
+    void testIsInvisibleItemFrame() {
+        // test with native item frame
+        ItemStack nativeFrame = new ItemStack(Material.ITEM_FRAME);
+        assert !Utils.isInvisibleItemFrame(nativeFrame, isInvisibleKey);
+
+        // test with native glow item frame
+        ItemStack nativeGlowFrame = new ItemStack(Material.GLOW_ITEM_FRAME);
+        assert !Utils.isInvisibleItemFrame(nativeGlowFrame, isInvisibleKey);
+
+        // test with regular invis item frame
+        ItemStack regInvisFrame = ItemFrameRegistry.getInstance().getRegularInvisibleFrame();
+        assert Utils.isInvisibleItemFrame(regInvisFrame, isInvisibleKey);
+
+        // test with glow invis item frame
+        ItemStack regGlowFrame = ItemFrameRegistry.getInstance().getGlowInvisibleFrame();
+        assert Utils.isInvisibleItemFrame(regGlowFrame, isInvisibleKey);
     }
 
-    @Disabled("TODO")
     @Test
     void testIsInvisibleItemFrameWithEntity() {
         World w = server.createWorld(new WorldCreator("world"));
@@ -60,14 +70,19 @@ class UtilsTest {
         Location loc = new Location(w, 0, 64, 0);
         Block b = w.getBlockAt(loc);
         b.setType(Material.STONE);
-        PlayerMock p = server.addPlayer();
-        w.spawnEntity(new Location(w, 0, 64, 0), EntityType.ITEM_FRAME);
 
+        // create a regular item frame entity
+        ItemFrame regularFrame = (ItemFrame) w.spawnEntity(loc, EntityType.ITEM_FRAME);
+        assert !Utils.isInvisibleItemFrame(regularFrame, isInvisibleKey);
+
+        // create an invisible item frame entity by adding the persistent data
+        ItemFrame invisibleFrame = (ItemFrame) w.spawnEntity(loc.add(1, 0, 0), EntityType.ITEM_FRAME);
+        invisibleFrame.getPersistentDataContainer().set(isInvisibleKey, org.bukkit.persistence.PersistentDataType.BYTE, (byte) 1);
+        assert Utils.isInvisibleItemFrame(invisibleFrame, isInvisibleKey);
+
+        // test with non-ItemFrame entity
+        org.bukkit.entity.Entity nonFrameEntity = w.spawnEntity(loc.add(2, 0, 0), EntityType.ZOMBIE);
+        assert !Utils.isInvisibleItemFrame(nonFrameEntity, isInvisibleKey);
     }
 
-    @Disabled("TODO")
-    @Test
-    void getNewMaterial() {
-
-    }
 }
