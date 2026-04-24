ca6723a861e8ffed9c1ee6fbda491323da332a97
Smoki <150018185+itzsmoki@users.noreply.github.com>
2026-04-24 19:05:25 +0200

    Fix chest reach problems

--- src/main/java/com/atlasgong/invisibleitemframeslite/listeners/ItemFrameInteractionListener.java
+++ src/main/java/com/atlasgong/invisibleitemframeslite/listeners/ItemFrameInteractionListener.java
@@ -6,11 +6,14 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Container;
+import org.bukkit.event.block.Action;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.RayTraceResult;
 
 /** Listener that handles interactions with invisible item frames. */
 public class ItemFrameInteractionListener implements Listener {
@@ -54,5 +57,36 @@ public class ItemFrameInteractionListener implements Listener {
             }
         }
     }
+
+    @EventHandler(priority = EventPriority.HIGH)
+        public void onPlayerInteractOutOfRange(PlayerInteractEvent e) {
+            // Intercept right clicks that fall through when the entity is out of the client reach limit
+            // Only works if you have something in your hand
+            if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                Player p = e.getPlayer();
+                
+                if (p.isSneaking()) return;
+
+                RayTraceResult rayTrace = p.getWorld().rayTraceEntities(
+                        p.getEyeLocation(),
+                        p.getEyeLocation().getDirection(),
+                        5.0,
+                        0.1,
+                        entity -> entity instanceof ItemFrame && Utils.isInvisibleItemFrame(entity, isInvisibleKey)
+                );
+
+                if (rayTrace != null && rayTrace.getHitEntity() != null) {
+                    ItemFrame frame = (ItemFrame) rayTrace.getHitEntity();
+                    
+                    BlockFace attachedFace = frame.getAttachedFace();
+                    Block mount = frame.getLocation().getBlock().getRelative(attachedFace);
+                    
+                    if (mount.getState() instanceof Container container) {
+                        e.setCancelled(true);
+                        p.openInventory(container.getInventory());
+                    }
+                }
+            }
+        }
 
 }
